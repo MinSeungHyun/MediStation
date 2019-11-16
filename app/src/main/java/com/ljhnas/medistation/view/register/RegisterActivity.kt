2160818bfc6ade11ef0350152577fd97797417e5
SeungHyunMin <asdf014563@naver.com>
2019-11-16 14:36:08 +0900

    RegisterActivity추가

---
+++ app/src/main/java/com/ljhnas/medistation/view/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.ljhnas.medistation.view.register
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ljhnas.medistation.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
