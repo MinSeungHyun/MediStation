f48eb1e680af205969c1b7acdb884bae5f806239
SeungHyunMin <asdf014563@naver.com>
2019-11-17 00:20:20 +0900

    SearchProgressActivity 생성

---
+++ app/src/main/java/com/ljhnas/medistation/view/search_progress/SearchProgressActivity.kt
@@ -0,0 +1,12 @@
+package com.ljhnas.medistation.view.search_progress
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ljhnas.medistation.R
+
+class SearchProgressActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_progress)
+    }
+}
