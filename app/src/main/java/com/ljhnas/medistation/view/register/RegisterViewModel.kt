d9409d7f9a6e3d2d63eb4e3352424298eccba3d6
SeungHyunMin <asdf014563@naver.com>
2019-11-16 21:54:19 +0900

    회원가입 페이지 구현

--- app/src/main/java/com/ljhnas/medistation/view/register/RegisterViewModel.kt
+++ app/src/main/java/com/ljhnas/medistation/view/register/RegisterViewModel.kt
@@ -1,17 +1,43 @@
 package com.ljhnas.medistation.view.register
 
 import android.app.Activity
+import androidx.databinding.ObservableArrayList
 import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
+import androidx.databinding.ObservableInt
 
 class RegisterViewModel(private val context: Activity) {
     val email: ObservableField<String> by lazy { ObservableField<String>() }
     val password: ObservableField<String> by lazy { ObservableField<String>() }
     val name: ObservableField<String> by lazy { ObservableField<String>() }
     val birthString: ObservableField<String> by lazy { ObservableField<String>() }
-    val isInvalid: ObservableBoolean by lazy { ObservableBoolean().apply { set(false) } }
+    val isFemale: ObservableBoolean by lazy { ObservableBoolean().apply { set(false) } }
+    val isPregnant: ObservableArrayList<Boolean> by lazy {
+        ObservableArrayList<Boolean>().apply {
+            add(true) //no
+            add(false) //pregnant
+            add(false) //lactating
+        }
+    }
+    val diseases: ObservableArrayList<Boolean> by lazy {
+        ObservableArrayList<Boolean>().apply {
+            add(true) //no
+            add(false) //고혈압
+            add(false) //당뇨
+        }
+    }
+    val takingMedicines: ObservableArrayList<Boolean> by lazy {
+        ObservableArrayList<Boolean>().apply {
+            add(true) //no
+            add(false) //당뇨약
+            add(false) //심작약
+        }
+    }
 
-    fun onRegisterButtonClick() {
+    val isInvalid: ObservableBoolean by lazy { ObservableBoolean().apply { set(false) } }
+    val currentStage: ObservableInt by lazy { ObservableInt().apply { set(0) } }
+
+    fun onNextButtonClick() {
         val email = email.get()
         val password = password.get()
         val name = name.get()
@@ -21,5 +47,18 @@ class RegisterViewModel(private val context: Activity) {
             return
         }
         isInvalid.set(false)
+
+        if (currentStage.get() == 4) {
+            val isFemale = if (isFemale.get()) "1" else "0"
+            val isPregnant = !isPregnant[0]
+            val allergy = ArrayList<String>()
+            if (diseases[1]) allergy.add("고혈압")
+            if (diseases[2]) allergy.add("당뇨")
+            val medicine = ArrayList<String>()
+            if (takingMedicines[1]) medicine.add("당뇨약")
+            if (takingMedicines[2]) medicine.add("심장약")
+        } else {
+            currentStage.set(currentStage.get() + 1)
+        }
     }
 }
