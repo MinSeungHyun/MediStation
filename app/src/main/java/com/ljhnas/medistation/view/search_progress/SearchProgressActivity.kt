cb1f14ed278ffd315742b9194e17f05114876063
SeungHyunMin <asdf014563@naver.com>
2019-11-17 01:43:39 +0900

    글씨 강조

--- app/src/main/java/com/ljhnas/medistation/view/search_progress/SearchProgressActivity.kt
+++ app/src/main/java/com/ljhnas/medistation/view/search_progress/SearchProgressActivity.kt
@@ -1,10 +1,13 @@
 package com.ljhnas.medistation.view.search_progress
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.style.ForegroundColorSpan
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ljhnas.medistation.R
 import com.ljhnas.medistation.databinding.ActivitySearchProgressBinding
+import kotlinx.android.synthetic.main.activity_search_progress.*
 
 class SearchProgressActivity : AppCompatActivity() {
     private val viewModel by lazy { SearchProgressViewModel(this) }
@@ -17,5 +20,12 @@ class SearchProgressActivity : AppCompatActivity() {
             vm = viewModel
             activity = this@SearchProgressActivity
         }
+
+        val span = mainText.text as Spannable
+        span.setSpan(ForegroundColorSpan(getColor(R.color.colorPrimary)), 15, 17, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mainText.text = span
+        val span2 = mainText2.text as Spannable
+        span2.setSpan(ForegroundColorSpan(getColor(R.color.colorPrimary)), 13, 15, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mainText2.text = span2
     }
 }
