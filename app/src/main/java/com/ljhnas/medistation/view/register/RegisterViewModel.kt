7fb5d835889eb80adf2c3491ff1a6d26018d6a1e
SeungHyunMin <asdf014563@naver.com>
2019-11-16 17:54:11 +0900

    모두 입력하지 않을 시 오류 텍스트 출력

--- app/src/main/java/com/ljhnas/medistation/view/register/RegisterViewModel.kt
+++ app/src/main/java/com/ljhnas/medistation/view/register/RegisterViewModel.kt
@@ -1,8 +1,25 @@
 package com.ljhnas.medistation.view.register
 
-import android.content.Context
+import android.app.Activity
+import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 
-class RegisterViewModel(private val context: Context) {
+class RegisterViewModel(private val context: Activity) {
+    val email: ObservableField<String> by lazy { ObservableField<String>() }
+    val password: ObservableField<String> by lazy { ObservableField<String>() }
+    val name: ObservableField<String> by lazy { ObservableField<String>() }
     val birthString: ObservableField<String> by lazy { ObservableField<String>() }
+    val isInvalid: ObservableBoolean by lazy { ObservableBoolean().apply { set(false) } }
+
+    fun onRegisterButtonClick() {
+        val email = email.get()
+        val password = password.get()
+        val name = name.get()
+        val birth = birthString.get()
+        if (email.isNullOrBlank() || password.isNullOrBlank() || name.isNullOrBlank() || birth.isNullOrBlank()) {
+            isInvalid.set(true)
+            return
+        }
+        isInvalid.set(false)
+    }
 }
