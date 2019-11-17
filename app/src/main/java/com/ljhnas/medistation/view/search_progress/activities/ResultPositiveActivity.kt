a2fbb2f51398fa13cc0742d3e82222c03b7b2889
SeungHyunMin <asdf014563@naver.com>
2019-11-17 09:34:11 +0900

    ResultPositiveActivity 생성

---
+++ app/src/main/java/com/ljhnas/medistation/view/search_progress/activities/ResultPositiveActivity.kt
@@ -0,0 +1,12 @@
+package com.ljhnas.medistation.view.search_progress.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ljhnas.medistation.R
+
+class ResultPositiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_positive)
+    }
+}
