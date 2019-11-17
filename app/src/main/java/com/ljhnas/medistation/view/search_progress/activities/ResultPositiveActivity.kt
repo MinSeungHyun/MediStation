ababdc239fb5f9c532139b2c255ee200201281e5
SeungHyunMin <asdf014563@naver.com>
2019-11-17 09:53:00 +0900

    ResultPositiveActivity 기본 구현

--- app/src/main/java/com/ljhnas/medistation/view/search_progress/activities/ResultPositiveActivity.kt
+++ app/src/main/java/com/ljhnas/medistation/view/search_progress/activities/ResultPositiveActivity.kt
@@ -1,12 +1,24 @@
 package com.ljhnas.medistation.view.search_progress.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ljhnas.medistation.R
+import kotlinx.android.synthetic.main.activity_result_negative.*
 
 class ResultPositiveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_positive)
+
+        goBackButton.setOnClickListener {
+            startActivity(Intent(this, SearchProgressActivity::class.java))
+            finish()
+        }
+        homeButton.setOnClickListener {
+            finish()
+        }
+
+
     }
 }
